c22d6da7f4a42d366f13ff066ea763e31b0542f8
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-05-16 17:28:43 +0100

    Stores the current date as shortened num

--- date.js
+++ date.js
@@ -6,3 +6,11 @@ tommorow.setDate(today.getDate() + 1);
 // Formats the date and outputs it to the webpage //
 const formattedDate = tommorow.toLocaleDateString();
 document.getElementById('date').textContent = formattedDate;
+
+// Gets the date shortend as a number //
+const day = tommorow.getDate().toString().padStart(2, '0');
+const month = tommorow.getMonth().toString().padStart(2, '0');
+const tagretNum = Number(day + month);
+
+// Outputs it to the console (temp) //
+console.log(tagretNum);
